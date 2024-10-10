51cbef7727ef95204105c40b7236129b29a4c38c
Ijharul <haqueijharul078@gmail.com>
2024-10-10 17:43:06 +0530

    bag

---
+++ scripts/bag.js
@@ -0,0 +1,57 @@
+let bagItemObjects = [];
+onLoad();
+
+function onLoad(){
+    loadBagItemsObjects();
+    displayBagItems();
+}
+
+function loadBagItemsObjects(){
+
+    bagItemObjects=bagItems.map(itemId =>{
+        for(let i =0; i<bagItems.length; i++){
+            if(itemId == items[i].Id){
+                return items[i];
+            }
+        }
+    });
+}
+
+function displayBagItems(){
+    let containerElement = document.querySelector('.bag-items-container');
+    let innerHTML = '';
+    bagItemObjects.forEach(bagItem => {
+        innerHTML += generateItemHTML(bagItem);
+
+    });
+    containerElement.innerHTML=innerHTML;
+}
+
+function generateItemHTML(item){
+    return `
+          <div class="bag-item-container">
+            <div class="item-left-part">
+               <img class="bag-item-img" src="${item.image}">
+            </div>
+            <div class="item-right-part">
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
+              <div class="price-container">
+                <span class="current-price">Rs${item.current_price}</span>
+                <span class="original-price">Rs${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+              </div>
+              <div class="return-period">
+                <span class="return-period-days">${item.return_period}</span> return available
+              </div>
+              <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${item.delivery_date}</span>
+              </div>
+            </div>
+
+            <div class="remove-from-cart">${item.id}</div>
+          
+`;
+
+}
